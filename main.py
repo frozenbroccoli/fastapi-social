80ea0d02085a029a495ff8f015ef5a1c4874ca51
Souvik Naskar <shouvik0104@gmail.com>
2024-05-05 23:16:11 +0530

    Added path operation function with post method. Saves the body of the request in a dict variable and prints it.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.params import Body
 
 app = FastAPI()
 
@@ -11,3 +12,9 @@ async def root():
 @app.get("/posts")
 def get_posts():
     return {"data": "These are your posts."}
+
+
+@app.post("/createposts")
+def create_posts(payload: dict = Body(...)):
+    print(payload)
+    return {"message": "Successfully created posts."}
