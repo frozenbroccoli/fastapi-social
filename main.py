ebefdf8c0625aefefabda0de233f0ac111b6035e
Souvik Naskar <shouvik0104@gmail.com>
2024-05-19 20:52:09 +0530

    created delete path operation function

--- main.py
+++ main.py
@@ -19,10 +19,10 @@ my_posts = []
 
 
 def find_post(post_id):
-    for post in my_posts:
+    for index, post in enumerate(my_posts):
         if post_id == post["id"]:
-            return post
-    return None
+            return index, post
+    return None, None
 
 
 @app.get("/")
@@ -37,7 +37,7 @@ def get_posts():
 
 @app.get("/posts/{post_id}")
 def get_post(post_id: int, response: Response):
-    post = find_post(post_id)
+    _, post = find_post(post_id)
 
     if post is None:
         raise HTTPException(
@@ -54,3 +54,17 @@ def create_posts(post: Post):
     post_dict["id"] = randrange(0, 100000)
     my_posts.append(post_dict)
     return {**post_dict}
+
+
+@app.delete("/posts/{post_id}", status_code=status.HTTP_200_OK)
+def delete_post(post_id: int, response: Response):
+    index, post = find_post(post_id)
+
+    if index is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"A post with post_id: {post_id} does not exist.",
+        )
+
+    my_posts.pop(index)
+    return {"message": "This post has been successfully deleted.", **post}
